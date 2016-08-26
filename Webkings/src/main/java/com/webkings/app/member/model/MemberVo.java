138c0bc0b847044fd16b92438d30c60bce3a9e85
User <User@UserPC43>
2016-08-26 18:15:09 +0900

    by 민혁 폴더 추가
    컨트롤러
    member/controller
    
    jsp 폴더
    member  
    common

---
+++ Webkings/src/main/java/com/webkings/app/member/model/MemberVo.java
@@ -0,0 +1,62 @@
+package com.webkings.app.member.model;
+
+import java.sql.Timestamp;
+
+public class MemberVo {
+	private int mNO;//회원번호 
+	private String mType;//회원구분값 
+	private String mEmail; //이메일
+	private String mNlck; //닉네임
+	private String mPwd;//비밀번호
+	private Timestamp mRegdate; //회원등록일
+	private Timestamp mChgdate; //회원수정일 
+	public int getmNO() {
+		return mNO;
+	}
+	public void setmNO(int mNO) {
+		this.mNO = mNO;
+	}
+	public String getmType() {
+		return mType;
+	}
+	public void setmType(String mType) {
+		this.mType = mType;
+	}
+	public String getmEmail() {
+		return mEmail;
+	}
+	public void setmEmail(String mEmail) {
+		this.mEmail = mEmail;
+	}
+	public String getmNlck() {
+		return mNlck;
+	}
+	public void setmNlck(String mNlck) {
+		this.mNlck = mNlck;
+	}
+	public String getmPwd() {
+		return mPwd;
+	}
+	public void setmPwd(String mPwd) {
+		this.mPwd = mPwd;
+	}
+	public Timestamp getmRegdate() {
+		return mRegdate;
+	}
+	public void setmRegdate(Timestamp mRegdate) {
+		this.mRegdate = mRegdate;
+	}
+	public Timestamp getmChgdate() {
+		return mChgdate;
+	}
+	public void setmChgdate(Timestamp mChgdate) {
+		this.mChgdate = mChgdate;
+	}
+	@Override
+	public String toString() {
+		return "MemberVo [mNO=" + mNO + ", mType=" + mType + ", mEmail=" + mEmail + ", mNlck=" + mNlck + ", mPwd="
+				+ mPwd + ", mRegdate=" + mRegdate + ", mChgdate=" + mChgdate + "]";
+	}
+	
+	
+}
