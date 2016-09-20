185e22c9306519976d0551f9ff10197963a0d2d7
User <User@UserPC96>
2016-09-20 23:44:14 +0900

    zz

--- Webkings/src/main/java/com/webkings/app/admin/controller/AdminItemController.java
+++ Webkings/src/main/java/com/webkings/app/admin/controller/AdminItemController.java
@@ -1,17 +1,148 @@
 package com.webkings.app.admin.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.webkings.app.item.model.ItemSearchVO;
+import com.webkings.app.item.model.ItemService;
+import com.webkings.app.item.model.ItemViewVO;
+import com.webkings.app.item.model.Item_TypeVO;
 
 @Controller
 public class AdminItemController {
 
+	private static final Logger logger= LoggerFactory.getLogger(AdminItemController.class);
+	
+	
+	@Autowired
+	private ItemService itemService;
+	
 	@RequestMapping("/adminItemView.do")
-	public String adminItemView(@RequestParam String gender, Model model){
-		
+	public String adminItemView(@RequestParam String gender,
+			  @RequestParam(defaultValue="ALL") String cate, @RequestParam(defaultValue="0") String orderVal, 
+			  @RequestParam(required=false) String sw2, @RequestParam(required=false) String ssp, @RequestParam(required=false) String sep,
+			  @RequestParam(required=false) String sac,  Model model){
+			if(orderVal==""){
+				orderVal="0";
+			}
+			List<Item_TypeVO> itemList = itemService.selectItemType(gender);
+			
+			ItemSearchVO itemSearchVo = new ItemSearchVO();
+			itemSearchVo.setSw2(sw2);
+			itemSearchVo.setSac(sac);
+			itemSearchVo.setCate(cate);
+			itemSearchVo.setSsp(ssp);
+			itemSearchVo.setSep(sep);
+			itemSearchVo.setOrderVal(orderVal);
+			
+			String orderName="";
+			if(orderVal.equals("0")){
+				orderName="신상품순";
+			}else if(orderVal.equals("1")){
+				orderName="클릭순";
+			}else if(orderVal.equals("2")){
+				orderName="높은 가격순";
+			}else if(orderVal.equals("3")){
+				orderName="낮은 가격순";
+			}
+			logger.info("itemSearchVo={}", itemSearchVo);
+			
+		model.addAttribute("cateView", cate);
+		model.addAttribute("itemList", itemList);
+		model.addAttribute("orderName", orderName);
+		model.addAttribute("itemSearchVo", itemSearchVo);
 		model.addAttribute("gender", gender);
 		return "page/admin/item";
+	}
+	
+	@RequestMapping("/adminItemSel.do")
+	@ResponseBody
+	public Map<String, Object> adItemSel(@RequestParam String gender, 
+			@RequestParam(defaultValue="ALL") String cate, @RequestParam String orderVal,
+			@RequestParam(required=false) String sw2, @RequestParam(required=false) String ssp, @RequestParam(required=false) String sep,
+			@RequestParam(required=false) String sac){
+			if(orderVal==""){
+				orderVal="0";
+			}
+			ItemViewVO itViewVo = new ItemViewVO();
+			itViewVo.setItGender(gender);
+			itViewVo.setItName(cate);
+			itViewVo.setsAge("");
+			logger.info("아이템 선택페이지에서 값들  cate,orderVal={},{}", cate,orderVal);
+			logger.info("gender, temViewVo={}", itViewVo);
+			
+			List<ItemViewVO> selItemList = null;
+			
+			ItemSearchVO itemSearchVo = new ItemSearchVO();
+			itemSearchVo.setSw2(sw2);
+			itemSearchVo.setSac(sac);
+			itemSearchVo.setCate(cate);
+			itemSearchVo.setSsp(ssp);
+			itemSearchVo.setSep(sep);
+			
+			if(!sw2.equals("") && !sw2.isEmpty() || !ssp.equals("") && !ssp.isEmpty() || !sep.equals("") && !sep.isEmpty() || !sac.equals("") && !sac.isEmpty()){
+				
+				if(sac.equals("1")){
+					sac="10";
+				}else if(sac.equals("2")){
+					sac="20";
+				}else if(sac.equals("3")){
+					sac="30";
+				}else{
+					sac=" ";
+				}
+				itemSearchVo.setSac(sac);
+				
+				logger.info("아이템 searchVo12={}", itemSearchVo);
+				selItemList = itemService.itemSearch(itemSearchVo);
+			}else{
+				itViewVo.setDateType("");	
+				if(orderVal.equals("0")){
+					if(cate.equals("ALL")){
+						selItemList= itemService.itemSelectAll(itViewVo);
+					}else{
+						selItemList= itemService.itemSelectName(itViewVo);
+					}
+				}
+				else if(orderVal.equals("1")){
+					if(cate.equals("ALL")){
+						selItemList= itemService.itemClick(itViewVo);
+					}else{
+						itViewVo.setItName(cate);
+						selItemList= itemService.itemClickCate(itViewVo);
+					}
+				}
+				else if(orderVal.equals("2")){
+					if(cate.equals("ALL")){
+						selItemList= itemService.itemPriceDesc(itViewVo);
+					}else{
+						selItemList= itemService.itemDescCate(itViewVo);
+					}
+				}else if(orderVal.equals("3")){
+					if(cate.equals("ALL")){
+						selItemList= itemService.itemPriceAsc(itViewVo);
+					}else{
+						selItemList= itemService.itemAscCate(itViewVo);
+					}
+				}
+			}
+			int itAllCount=itemService.itemAllCount();
+			
+			
+			Map<String, Object> resMap = new HashMap<String, Object>();
+			resMap.put("itemList", selItemList);
+			resMap.put("itAllCount", itAllCount);
+			logger.info("ajax itemList={}", selItemList);
+			return resMap;
 	}
 }
--- Webkings/src/main/webapp/js/adminItem.js
+++ Webkings/src/main/webapp/js/adminItem.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
 				async:false,
 				dataType:"json",
 				success:function(res){
-					itCount = res.itCount;
 					itemSel = res.itemList;
 					itAllCount = res.itAllCount;
 					var result="";
@@ -20,30 +19,16 @@ $(document).ready(function() {
 					$.each(itemSel, function(idx, item) {
 					result+=
 					"<li class='prod "+gender+"'>"+
-						"<a id='aaa1' href='http://"+item.iDomain+"'  target='_blank' sseq='121' seq='1349867' maindate='20160908'>"+
+						"<a id='aaa1' href='javascript:;'  sseq='121' seq='1349867' maindate='20160908'>"+
 						"<input type='hidden' id='itemiNo' name='iINo' value='"+item.iNo+"'>"+
 							"<img src='/Webkings/ItemImage/"+item.iImage+"' data-original='http://img.sta1.kr/_up/prod/main/2016/09/08/1473208334629_w.jpg'"+
 							"style='height: 340px; display: block;' class='item'>"+
-
-							"<span class='favor'>관심상품<input type='hidden' id='inputiNo' name='iINo' value='"+item.iNo+"'>" +
-										"<input type='hidden' id='inputsNo' name='iSNo' value='"+item.sNo+"'></span>"+
-
-							"<span class='favor' id='addItem'>관심상품<input type='hidden' id='inputiNo' name='iINo' value='"+item.iNo+"'></span>"+
 							"<div class='info'>	<span class='shop'>"+item.sName+"</span>"+		
 								"<span class='name'>"+item.iName+"</span>	<em class='cate' cate='101'>"+item.itName+"</em><i>"+
 								item.iSalePrice+"</i>"+		
 								"<div class='btn'>"+
-									"<span class='fb'>" +
-									"<input type='hidden' id='fbTitle' value='"+item.iName+"'/>"+
-									"<input type='hidden' id='fbUrl' value='"+item.iDomain+"'/>" +
-									"<input type='hidden' id='fbImage' value='"+item.iImage+"'/>" +
-									"<input type='hidden' id='fbContent' value='"+item.itName+"'/>" +
-									"</span><i>페이스북 공유</i>" +
-									"<span class='tw'>" +
-										"<input type='hidden' id='itN' value='"+item.iName+"'/>"+
-										"<input type='hidden' id='doma' value='"+item.iDomain+"'/>" +
-									"</span>" +
-									"<i>트위터 공유</i>"+
+									"<span class='fb'><input type='hidden' id='inputiNo' name='iINo' value='"+item.iNo+"'>" +
+									"</span><i>삭제</i>" +
 								"</div>"+	
 							"</div>"+	
 							"<div class='bg'></div>"+
@@ -58,17 +43,15 @@ $(document).ready(function() {
 			}); 
 			 
 			$(".notice-2 em").append(itAllCount);
-			$(".item-list>li.tit-cate>div strong").append(itemCate); 
-			$(".item-list>li.tit-cate>div span em").append(itCount);
 			 
 		$(".option-sec-1>ul>li.order ul a").click(function(){
 					orderVal=$(this).find("input").val();
-					$(location).attr('href', "/Webkings/item/itemCate.do?page=/product&gender="+gender+"&cate="+itemCate+"&orderVal="+orderVal+"&sw2="+sw2+"&ssp="+ssp+"&sep="+sep+"&sac="+sac);
+					$(location).attr('href', "/Webkings/adminItemView.do?gender="+gender+"&cate="+itemCate+"&orderVal="+orderVal+"&sw2="+sw2+"&ssp="+ssp+"&sep="+sep+"&sac="+sac);
 				});
 				
 		$("a.aw-1").append(orderName);
 		$("a.aw-1").click(function(){
-			$(location).attr('href', "/Webkings/item/itemCate.do?page=/product&gender="+gender+"&cate="+itemCate+"&orderVal="+orderVal+"&sw2="+sw2+"&ssp="+ssp+"&sep="+sep+"&sac="+sac);
+			$(location).attr('href', "/Webkings/adminItemView.do?gender="+gender+"&cate="+itemCate+"&orderVal="+orderVal+"&sw2="+sw2+"&ssp="+ssp+"&sep="+sep+"&sac="+sac);
 		});
 		
 		/*검색창 값넣기*/
@@ -100,7 +83,6 @@ $(document).ready(function() {
 		}
 		$(".option-sec-1>ul>li.detail-search .sec div #orderVal").val(orderVal);
 		
-		
 		$(".option-sec-1>ul>li.detail-search .sec div.btn a.reset").click(function() {
 			$(".option-sec-1>ul>li.detail-search .sec p input").val("");
 			$(".option-sec-1>ul>li.detail-search .sec div input[name=ssp]").val("");
@@ -110,16 +92,4 @@ $(document).ready(function() {
 			$(".option-sec-1>ul>li.detail-search .sec div ul li input[id=age2]").prop("checked", false);
 			$(".option-sec-1>ul>li.detail-search .sec div ul li input[id=age3]").prop("checked", false);
 		});
-		
-
 });
-function gbn(){
-	if($(".list-top-1 p").hasClass("notice-1")){
-		page="/product";
-		$(location).attr('href', "/Webkings/item/itemCate.do?page="+page+"&gender="+gender);
-		
-	}else if($(".list-top-1 p").hasClass("notice-2")){
-		page="/item";
-		$(location).attr('href', "/Webkings/page.do?page="+page+"&gender="+gender);
-	}
-}
