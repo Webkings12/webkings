a35495b70f84d53a053e2cab1b15f3e3bf4141e5
User <User@UserPC96>
2016-09-21 18:48:17 +0900

    sss

--- Webkings/src/main/java/com/webkings/app/chief/model/ChiefDAO.java
+++
@@ -1,7 +0,0 @@
-package com.webkings.app.chief.model;
-
-public interface ChiefDAO {
-	public int loginCheck(ChiefVO vo);
-	public int selectChiefId(String reqParam);
-	public int insertChief(ChiefVO vo);	
-}
--- Webkings/src/main/java/com/webkings/app/chief/model/ChiefService.java
+++
@@ -1,7 +0,0 @@
-package com.webkings.app.chief.model;
-
-public interface ChiefService {
-	public int loginCheck(ChiefVO vo);
-	public int insertChief(ChiefVO vo);
-	public int selectChiefId(String reqParam);
-}
